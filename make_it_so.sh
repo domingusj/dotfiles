08780e07b72d290abd9ea51a2f1125ca3c5ca42c
Justin Domingus <domingusj@gmail.com>
2015-10-10 15:35:59 -0600

    remove homebrew from start script

--- make_it_so.sh
+++ make_it_so.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apm stars --user domingusj --install;
-mv ~/.tmux.conf ~/.tmux.bak;
-ln -sn ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf;
+-mv ~/.tmux.conf ~/.tmux.bak;
+-ln -sn ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf;
 mv ~/.vimrc ~/.vimrc.bak;
 ln -sn ~/projects/dotfiles/vim/vimrc.symlink ~/.vimrc;
 mkdir -p ~/.vim/colors;
@@ -10,4 +10,3 @@ cp ~/projects/dotfiles/vim/colors/solarized.vim ~/.vim/colors;
 mv ~/.zshrc ~/.zshrc.bak;
 ln -sn ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc;
 ~/projects/dotfiles/osx/set-defaults.sh;
-~/projects/dotfiles/homebrew/install.sh;
