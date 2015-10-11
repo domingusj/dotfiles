3ea26f5f4c9165c5fa484fb03efd337a094df0f2
Justin Domingus <domingusj@gmail.com>
2015-10-10 18:49:56 -0600

    add agnoester short theme and update start script

--- make_it_so.sh
+++ make_it_so.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apm stars --user domingusj --install;
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 -mv ~/.tmux.conf ~/.tmux.bak;
 -ln -sn ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf;
 mv ~/.vimrc ~/.vimrc.bak;
@@ -9,4 +10,5 @@ mkdir -p ~/.vim/colors;
 cp ~/projects/dotfiles/vim/colors/solarized.vim ~/.vim/colors;
 mv ~/.zshrc ~/.zshrc.bak;
 ln -sn ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc;
+cp ~/projects/dotfiles/zsh/agnoster-short.zsh-theme zshrc.symlink ~/.oh-my-zsh/themes/
 ~/projects/dotfiles/osx/set-defaults.sh;
