85b4971b4223bb2377596d004890c34c0b8658a8
Justin Domingus <domingusj@gmail.com>
2015-10-10 18:58:07 -0600

    add homebrew

--- make_it_so.sh
+++ make_it_so.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 apm stars --user domingusj --install;
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--mv ~/.tmux.conf ~/.tmux.bak;
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+mv ~/.tmux.conf ~/.tmux.bak;
 -ln -sn ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf;
 mv ~/.vimrc ~/.vimrc.bak;
 ln -sn ~/projects/dotfiles/vim/vimrc.symlink ~/.vimrc;
@@ -10,7 +11,7 @@ mkdir -p ~/.vim/colors;
 cp ~/projects/dotfiles/vim/colors/solarized.vim ~/.vim/colors;
 mv ~/.zshrc ~/.zshrc.bak;
 ln -sn ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc;
-ln -sn ~/projects/dotfiles/zsh/agnoster-short.zsh-theme ~/.oh-my-zsh/themes/agnoster-short.zsh-theme
+ln -sn ~/projects/dotfiles/zsh/agnoster-short.zsh-theme ~/.oh-my-zsh/themes/agnoster-short.zsh-theme;
 ~/projects/dotfiles/osx/set-defaults.sh;
-source ~/.zshrc
+source ~/.zshrc;
 
