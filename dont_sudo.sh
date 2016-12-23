d55af197d624b385045e0a874d12eb56ec1a0f1f
Justin Domingus <domingusj@gmail.com>
2016-12-23 07:03:20 -0800

    Bootstrap cleanup, use vim

--- dont_sudo.sh
+++ dont_sudo.sh
@@ -1,84 +1,82 @@
 #!/bin/bash
 # make sure XCode is working first
 sudo xcodebuild -license
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; # oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # oh-my-zsh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # Homebrew
-brew update;
-brew doctor;
-brew tap caskroom/cask;
+brew update
+brew doctor
+brew tap caskroom/cask
 # brew packages
-brew cask install google-chrome;
-brew cask install google-play-music-desktop-player;
-brew cask install virtualbox;
-brew cask install iterm2;
-brew cask install slack;
-brew cask install dropbox;
-brew cask install 1password;
-brew tap ravenac95/sudolikeaboss;
-brew install sudolikeaboss;
-brew cask install flux;
-brew cask install utc-menu-clock;
-brew cask install spectacle;
-brew cask install screenhero;
-brew tap caskroom/fonts;
-brew cask install font-inconsolata;
-brew cask install docker;
-bew cask install gpgtools;
+brew cask install 1password
+brew cask install docker
+brew cask install dropbox
+brew cask install flux
+brew cask install google-chrome
+brew cask install google-play-music-desktop-player
+brew cask install gpgtools
+brew cask install iterm2
+brew cask install screenhero
+brew cask install slack
+brew cask install spectacle
+brew cask install utc-menu-clock
+brew cask install virtualbox
+brew tap ravenac95/sudolikeaboss
+brew install sudolikeaboss
+brew tap caskroom/fonts
+brew cask install font-inconsolata
 # shell packages
-brew install git;
-brew install hub;
-brew install tree;
-brew install diff-so-fancy;
-brew install gpg;
-brew install git;
-brew install wget;
-brew install tmux;
-brew install ncdu;
-brew install transcrypt;
-brew install reattach-to-user-namespace;
-brew install homebrew/gui/meld;
-brew install watch;
-brew install jq;
-brew install htop;
+brew install diff-so-fancy
+brew install git
+brew install git
+brew install gpg
+brew install homebrew/gui/meld
+brew install htop
+brew install hub
+brew install jq
+brew install ncdu
+brew install reattach-to-user-namespace
+brew install tmux
+brew install transcrypt
+brew install tree
+brew install vim
+brew install watch
+brew install wget
 # cloud tools
-brew cask install google-cloud-sdk;
-brew install awscli;
-gcloud components install kubectl;
+brew install awscli
+brew cask install google-cloud-sdk
+gcloud components install kubectl
 # Hashicorp
-brew install ansible;
-brew cask install vagrant;
-brew install vagrant-completion;
-brew cask install terraform;
-brew install packer;
-brew install packer-completion;
+brew install ansible
+brew cask install terraform
+brew cask install vagrant
+brew install vagrant-completion
+brew install packer
+brew install packer-completion
 # languages
-brew cask install puppet;
-gem install puppet-lint;
-brew install ruby;
-gem install bundle;
-brew install python;
-brew install python3;
-brew install npm;
-brew install node;
-brew install ruby;
+brew cask install puppet
+brew install node
+brew install npm
+brew install python3
+brew install python
+brew install ruby
+gem install bundle
+gem install puppet-lint
 \curl -sSL https://get.rvm.io | bash # RVM
-mkdir ~/go;
-brew install go;
-# neovim
-# export SED=$(which sed) - if there are problems installing neovim
-brew install neovim/neovim/neovim;
-mkdir -p ~/.config/nvim;
+mkdir ~/go
+brew install go
+# vim
+mv ~/.vimrc ~/.vimrc.bak
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sn ~/projects/dotfiles/vim/vimrc.symlink ~/.config/nvim/init.vim;
+ln -s ~/projects/dotfiles/vim/vimrc.symlink ~/.vimrc
 # atom config
-brew cask install atom;
-mkdir ~/.atom;
-atom ~/;
-apm stars --user domingusj --install;
-apm disable wrap-guide;
-mv ~/.atom/keymap.cson ~/.atom/keymap.cson.bak;
-mv ~/.atom/config.cson ~/.atom/config.cson.bak;
-ln -s ~/projects/dotfiles/atom/keymap.cson ~/.atom/keymap.cson;
-ln -s ~/projects/dotfiles/atom/config.cson ~/.atom/config.cson;
-exit 0;
+brew cask install atom
+mkdir ~/.atom
+atom ~/ # open atom to initialize
+apm stars --user domingusj --install
+apm disable wrap-guide
+mv ~/.atom/keymap.cson ~/.atom/keymap.cson.bak
+mv ~/.atom/config.cson ~/.atom/config.cson.bak
+ln -s ~/projects/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
+ln -s ~/projects/dotfiles/atom/config.cson ~/.atom/config.cson
+exit 0
--- symlink.sh
+++ symlink.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # symlink dotfiles
-mkdir ~/projects ~/ansible;
-ln -sn ~/projects/dotfiles/ansible/ansible.cfg ~/ansible.cfg;
-mkdir ~/.ssh;
-ln -sn ~/projects/dotfiles/ssh/config ~/.ssh/config;
-mv ~/.tmux.conf ~/.tmux.bak;
-ln -sn ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf;
-mv ~/.puppet-lint.rc ~/.puppet-lint.rc.bak;
-ln -sn ~/projects/dotfiles/puppet/puppet-lint.rc.symlink ~/.puppet-lint.rc;
-mv ~/.gitconfig ~/.gitconfig.bak;
-ln -sn ~/projects/dotfiles/git/.gitconfig.symlink ~/.gitconfig;
-mv ~/.zshrc ~/.zshrc.bak;
-ln -sn ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc;
-ln -sn ~/projects/dotfiles/zsh/af-magic-short.zsh-theme ~/.oh-my-zsh/themes/af-magic-short.zsh-theme;
+mkdir ~/projects ~/ansible
+ln -s ~/projects/dotfiles/ansible/ansible.cfg ~/ansible.cfg
+mkdir ~/.ssh
+ln -s ~/projects/dotfiles/ssh/config ~/.ssh/config
+mv ~/.tmux.conf ~/.tmux.bak
+ln -s ~/projects/dotfiles/tmux/tmux.symlink ~/.tmux.conf
+mv ~/.puppet-lint.rc ~/.puppet-lint.rc.bak
+ln -s ~/projects/dotfiles/puppet/puppet-lint.rc.symlink ~/.puppet-lint.rc
+mv ~/.gitconfig ~/.gitconfig.bak
+ln -s ~/projects/dotfiles/git/.gitconfig.symlink ~/.gitconfig
+mv ~/.zshrc ~/.zshrc.bak
+ln -s ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc
+ln -s ~/projects/dotfiles/zsh/af-magic-short.zsh-theme ~/.oh-my-zsh/themes/af-magic-short.zsh-theme
 # zsh / shell config
-cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git;
+cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 touch ~/.hushlogin # disable iTerm login message
-exit 0;
+exit 0
