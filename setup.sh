1e4975d9f14e099a1e5fc7fd19c532b68a48bbc4
Justin Domingus <domingusj@gmail.com>
2021-09-28 14:11:04 -0700

    Update setup script - fix path

--- setup.sh
+++ setup.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # pyenv
 curl https://pyenv.run | bash
-
-# rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/projects/dotfiles/vim/vimrc.symlink ~/.vimrc
+ln -s $(pwd)/vim/vimrc.symlink ~/.vimrc
 
 # symlink dotfiles
-ln -s ~/projects/dotfiles/ssh/config ~/.ssh/config
-ln -s ~/projects/dotfiles/git/.gitconfig.symlink ~/.gitconfig
+ln -s $(pwd)/ssh/config ~/.ssh/config
+ln -s $(pwd)/git/.gitconfig.symlink ~/.gitconfig
 mv ~/.zshrc ~/.zshrc.bak
-ln -s ~/projects/dotfiles/zsh/zshrc.symlink ~/.zshrc
-ln -s ~/projects/dotfiles/zsh/af-magic-short.zsh-theme ~/.oh-my-zsh/themes/af-magic-short.zsh-theme
+ln -s $(pwd)/zsh/zshrc.symlink ~/.zshrc
+ln -s $(pwd)/zsh/af-magic-short.zsh-theme ~/.oh-my-zsh/themes/af-magic-short.zsh-theme
 
 # zsh / shell config
 cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# vscode
-echo "Setup Settings Sync for vscode and download https://gist.github.com/domingusj/5c569896ea3ffc78db3949b72e431231"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
